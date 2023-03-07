ab443d1769c7f80feadbb7ee68ced0c1b8ec5796
sumicet <mihailescuc@outlook.com>
2023-03-07 18:50:49 +0200

    Add Image

---
+++ src/components/Image.tsx
@@ -0,0 +1,13 @@
+import type { ImageProps as ChakraImageProps } from '@chakra-ui/image';
+import { Image as ChakraImage } from '@chakra-ui/image';
+
+const fallbackSrc = 'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=';
+
+export type ImageProps = ChakraImageProps;
+
+/**
+ * Wraps Chakra's Image component.
+ */
+export function Image({ src, ...rest }: ImageProps) {
+    return <ChakraImage src={src || fallbackSrc} {...rest} />;
+}
