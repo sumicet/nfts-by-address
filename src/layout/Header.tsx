1629e17efcbf5acbb8774c343a66ade3261dc9e5
sumicet <mihailescuc@outlook.com>
2023-03-07 17:24:04 +0200

    Add Header

---
+++ src/layout/Header.tsx
@@ -0,0 +1,26 @@
+import { Image } from '@chakra-ui/image';
+import { HStack } from '@chakra-ui/layout';
+import { useColorMode, useColorModeValue } from '@chakra-ui/system';
+import { DarkModeSwitch } from 'react-toggle-dark-mode';
+
+export function Header() {
+    const { colorMode, toggleColorMode } = useColorMode();
+    const shadow = useColorModeValue('logo.light', 'logo.dark');
+    return (
+        <HStack justifyContent="space-between" height="header" width="100%" paddingX="space30">
+            <Image
+                src="https://i.imgur.com/riyzh0s.gif"
+                boxSize="space40"
+                borderRadius="radius4"
+                boxShadow={shadow}
+            />
+            <DarkModeSwitch
+                checked={colorMode === 'light'}
+                onChange={toggleColorMode}
+                sunColor="white"
+                moonColor="black"
+                size={25}
+            />
+        </HStack>
+    );
+}
--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,11 +1,15 @@
-import { Box, Center, type CenterProps } from '@chakra-ui/layout';
+import { Box, Center, VStack, type CenterProps } from '@chakra-ui/layout';
+import { Header } from './Header';
 
 export function Layout({ children, ...rest }: CenterProps) {
     return (
-        <Center boxSize="100%" {...rest}>
-            <Box boxSize="100%" maxWidth="1400px" paddingX="2rem">
-                {children}
-            </Box>
-        </Center>
+        <VStack boxSize="100%" {...rest}>
+            <Header />
+            <Center boxSize="100%">
+                <Box boxSize="100%" maxWidth="1400px" paddingX="2rem">
+                    {children}
+                </Box>
+            </Center>
+        </VStack>
     );
 }
