707860a395a472deb8e1a390c629c9a0fb257ab9
sumicet <mihailescuc@outlook.com>
2023-03-08 01:18:14 +0200

    Fix grid ts issue

--- src/components/InfiniteGridItem.tsx
+++ src/components/InfiniteGridItem.tsx
@@ -1,12 +1,12 @@
 import { Box } from '@chakra-ui/layout';
 import { forwardRef } from 'react';
-import { type ItemProps } from 'react-virtuoso';
+import { type GridItemProps } from 'react-virtuoso';
 
 /**
  * Used for the Infinite Grid.
  * Needed to set the width to 100%.
  */
-export const InfiniteGridItem = forwardRef<HTMLDivElement, ItemProps<any>>((props, ref) => (
+export const InfiniteGridItem = forwardRef<HTMLDivElement, GridItemProps>((props, ref) => (
     <Box {...props} ref={ref} boxSize="100%" />
 ));
 
